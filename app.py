a7e215346dcdabd3d1c7ee944be3df346f479522
andrew_swartzz <andreww_swartzz@gmail.com>
2025-04-08 13:18:03 -0400

    created base UI

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return render_template('test.html')
-
-
-@app.route('/update_pixel', methods=['POST'])
-def update_pixel():
-    data = request.get_json()
-    row = data['row']
-    col = data['col']
-    color = data['color']
-    # Here you would add logic to store the pixel data, e.g., in a list or databas
-    return jsonify({'status': 'success'})
-
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
