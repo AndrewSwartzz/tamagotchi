1ebd99f0b179ec2879e5ba1cc6de3301310c1d33
connorkearns <ckearns1@ithaca.edu>
2025-04-18 12:44:39 -0400

    another push

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 
 from forms import RegistrationForm
-from app.models import User
+from app.models import Us
 from app import db, app
 
 
